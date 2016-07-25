ec12b35a36073a480a5bb33f70d661afece75bc7
Mihail Guschin <mikhail91@mikhail91-osx.local>
2016-07-25 23:47:26 +0300

    New Tracks Combination

--- functions/combination.py
+++ functions/combination.py
@@ -152,3 +152,182 @@ class Combinator(object):
         self.tracks_combinations_ = tracks_combinations
         self.charges_ = charges
         self.inv_momentums_ = inv_momentums
+
+
+class SuperCombinator(Combinator):
+
+    """
+        This class combines tracks before and after the magnet using a classifier,
+        estimates a particles charge and momentim based on its deflection in the magnetic field.
+        :params classifier: scikit-learn-like classifier, which predicts that
+        two tracks candidates before and after the magnet belongs the same track
+        :param z_magnet: floaf, z-coordinate of the center of the magnet.
+        :param magnetic_field: float, inductivity of the magnetic field.
+        :param dy_max: float, max distance on y between the tracks before and after the magnet in center of the magnet.
+        :param dx_max: float, max distance on x between the tracks before and after the magnet in center of the magnet.
+        :return:
+        """
+
+    def __init__(self, classifier=None, z_magnet=3070., magnetic_field=-0.75):
+
+        Combinator.__init__(self, z_magnet, magnetic_field)
+
+        self.classifier = classifier
+
+    def data_collection(self, tracks, events):
+        """
+        The method returns data for the classifier training.
+        :param tracks: dict, {event_id: 'labels12':[-1, 1, 0, -1, ...], 'params12':[[[k_y, b_y], [k_stereo, b_stereo]], ...],
+                           'labels34':[-1, 1, 0, -1, ...], 'params34':[[[k_y, b_y], [k_stereo, b_stereo]], ...]}
+        :param events: pandas.DataFrame(), True MC of the hits.
+        :return: pandas.DataFrame()
+        """
+
+        combination_data = pandas.DataFrame(columns=[u'dx', u'dy', 'dr',
+                                                      u'k_xz_12', u'b_xz_12',
+                                                      u'k_yz_12',u'b_yz_12',
+                                                      u'k_xz_34', u'b_xz_34',
+                                                      u'k_yz_34', u'b_yz_34',
+                                                      'dk_xz', 'db_xz',
+                                                      'dk_yz', 'db_yz', 'label'])
+
+        for event_id in tracks.keys():
+
+            event = events[events.EventID == event_id]
+            track = tracks[event_id]
+
+            event12 = event[(event.StatNb == 1) + (event.StatNb == 2)]
+            event34 = event[(event.StatNb == 3) + (event.StatNb == 4)]
+
+
+            true_labels12 = event12.Label.values
+            true_labels34 = event34.Label.values
+
+            labels12 = track['labels12']
+            labels34 = track['labels34']
+            params12 = track['params12']
+            params34 = track['params34']
+
+            for before in range(len(params12)):
+
+                for after in range(len(params34)):
+
+                    if len(params12[before][1]) == 0 or len(params34[after][1]) == 0:
+                        continue
+
+
+                    k_xz_12, b_xz_12 = params12[before][1][0], params12[before][1][1]
+                    k_yz_12, b_yz_12 = params12[before][0][0], params12[before][0][1]
+                    k_xz_34, b_xz_34 = params34[after][1][0], params34[after][1][1]
+                    k_yz_34, b_yz_34 = params34[after][0][0], params34[after][0][1]
+
+                    dk_xz = numpy.abs(params34[after][1][0] - params12[before][1][0])
+                    db_xz = numpy.abs(params34[after][1][1] - params12[before][1][1])
+
+                    dk_yz = numpy.abs(params34[after][0][0] - params12[before][0][0])
+                    db_yz = numpy.abs(params34[after][0][1] - params12[before][0][1])
+
+                    y_before = self.z_magnet * k_yz_12 + b_yz_12
+                    x_before = self.z_magnet * k_xz_12 + b_xz_12
+
+                    y_after = self.z_magnet * k_yz_34 + b_yz_34
+                    x_after = self.z_magnet * k_xz_34 + b_xz_34
+
+                    dy = numpy.abs(y_after - y_before)
+                    dx = numpy.abs(x_after - x_before)
+                    dr = numpy.sqrt(dx**2 + dy**2)
+
+
+                    unique_before, counts_before = numpy.unique(true_labels12[labels12 == before],
+                                                                return_counts=True)
+                    max_fraction_true_label_before = unique_before[counts_before == counts_before.max()][0]
+
+                    unique_after, counts_after = numpy.unique(true_labels34[labels34 == after],
+                                                              return_counts=True)
+                    max_fraction_true_label_after = unique_after[counts_after == counts_after.max()][0]
+
+                    if max_fraction_true_label_before == max_fraction_true_label_after:
+                        label = 1
+                    else:
+                        label = 0
+
+                    combination_data.loc[len(combination_data)] = [dx, dy, dr,
+                                                                   k_xz_12, b_xz_12,
+                                                                   k_yz_12, b_yz_12,
+                                                                   k_xz_34, b_xz_34,
+                                                                   k_yz_34, b_yz_34,
+                                                                   dk_xz, db_xz,
+                                                                   dk_yz, db_yz, label]
+
+        return combination_data
+
+
+    def get_tracks_combination(self, tracks_before, tracks_after):
+        """
+        This function combines the two tracks.
+        :param tracks_before: list of [[k_yz, b_yz], [k_xz, b_xz]], parameters of the track before the magnet. y = kx + b.
+        :param tracks_after: list, [[k_yz, b_yz], [k_xz, b_xz]], parameters of the track after the magnet. y = kx + b.
+        :return: list of [track_id_before, track_id_after]
+        """
+
+        z_magnet = self.z_magnet
+        dy_max = self.dy_max
+        dx_max = self.dx_max
+
+        tracks_combinations = []
+
+        for track_id_before, one_track_before in enumerate(tracks_before):
+
+            for track_id_after, one_track_after in enumerate(tracks_after):
+
+
+                if len(one_track_before[1])==0 or len(one_track_after[1])==0:
+                    continue
+
+
+                k_xz_12, b_xz_12 = one_track_before[1][0], one_track_before[1][1]
+                k_yz_12, b_yz_12 = one_track_before[0][0], one_track_before[0][1]
+                k_xz_34, b_xz_34 = one_track_after[1][0], one_track_after[1][1]
+                k_yz_34, b_yz_34 = one_track_after[0][0], one_track_after[0][1]
+
+                dk_xz = numpy.abs(k_xz_34 - k_xz_12)
+                db_xz = numpy.abs(b_xz_34 - b_xz_12)
+
+                dk_yz = numpy.abs(k_yz_34 - k_yz_12)
+                db_yz = numpy.abs(b_yz_34 - b_yz_12)
+
+                y_before = self.z_magnet * k_yz_12 + b_yz_12
+                x_before = self.z_magnet * k_xz_12 + b_xz_12
+
+                y_after = self.z_magnet * k_yz_34 + b_yz_34
+                x_after = self.z_magnet * k_xz_34 + b_xz_34
+
+                y_before = z_magnet * one_track_before[0][0] + one_track_before[0][1]
+                x_before = z_magnet * one_track_before[1][0] + one_track_before[1][1]
+
+                y_after = z_magnet * one_track_after[0][0] + one_track_after[0][1]
+                x_after = z_magnet * one_track_after[1][0] + one_track_after[1][1]
+
+                dy = numpy.abs(y_after - y_before)
+                dx = numpy.abs(x_after - x_before)
+                dr = numpy.sqrt(dy**2 + dx**2)
+
+                #print dy, dx
+
+                x_clf = numpy.array([[dx, dy, dr,
+                                   k_xz_12, b_xz_12,
+                                   k_yz_12, b_yz_12,
+                                   k_xz_34, b_xz_34,
+                                   k_yz_34, b_yz_34,
+                                   dk_xz, db_xz,
+                                   dk_yz, db_yz]])
+
+                label = self.classifier.predict(x_clf)
+
+
+                if label[0] == 1:
+
+                    tracks_combinations.append(numpy.array([track_id_before, track_id_after]))
+                    continue
+
+        return numpy.array(tracks_combinations)
